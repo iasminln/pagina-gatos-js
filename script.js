584e5159072a885a495b2af8c610b7155c0f0d2a
Iasmin <iasmin.nogueira@penseavanti.com.br>
2022-04-13 20:46:09 -0300

    adicionado dark mode e aumento de fonte

--- script.js
+++ script.js
@@ -1,40 +1,71 @@
-function ClickFotos(){
+function ClickFotos() {
   const tabMenu = document.querySelectorAll(
     ".js-foto-descricao .fotos-gatos img"
   );
   const tabContent = document.querySelectorAll(".js-foto-descricao .item-gato");
-  
-  tabContent[0].classList.add('ativo')
-  
-  function clickTab(index){
-    tabContent.forEach((itemContent)=>{
-      itemContent.classList.remove('ativo')
-    })
-    tabContent[index].classList.add('ativo')
+
+  tabContent[0].classList.add("ativo");
+
+  function clickTab(index) {
+    tabContent.forEach((itemContent) => {
+      itemContent.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
   }
-  
+
   tabMenu.forEach((item, index) => {
-    item.addEventListener("click", ()=>{
-      clickTab(index)
+    item.addEventListener("click", () => {
+      clickTab(index);
     });
   });
-  
 }
 
-ClickFotos()
+ClickFotos();
 
-function ActiveDarkMode(){
-  const imgMode = document.querySelector('.img-dark-light')
-  const htmlDoc = document.querySelector('html')
-  
-  
-  function clickImgMode(){
-    htmlDoc.classList.toggle('dark-ativo')
+function ActiveDarkMode() {
+  const imgMode = document.querySelectorAll(".img-dark-light");
+  const htmlDoc = document.querySelector("html");
+
+  function clickImgMode() {
+    htmlDoc.classList.toggle("dark-ativo");
+
+    imgMode.forEach((item) => {
+      if (item.classList.contains("ativo")) {
+        item.classList.remove("ativo");
+      } else {
+        item.classList.add("ativo");
+      }
+    });
   }
-  
-  imgMode.addEventListener("click", clickImgMode)
+
+  imgMode.forEach((item) => {
+    item.addEventListener("click", () => {
+      clickImgMode();
+    });
+  });
 }
-ActiveDarkMode()
+ActiveDarkMode();
 
+function IncreaseText() {
+  const textMode = document.querySelectorAll(".img-text");
+  const bodyDoc = document.querySelector("body");
 
+  function clickTextMode() {
+    bodyDoc.classList.toggle("letra-maior");
 
+    textMode.forEach((item) => {
+      if (item.classList.contains("ativo")) {
+        item.classList.remove("ativo");
+      } else {
+        item.classList.add("ativo");
+      }
+    });
+  }
+
+  textMode.forEach((item) => {
+    item.addEventListener("click", () => {
+      clickTextMode();
+    });
+  });
+}
+IncreaseText();
